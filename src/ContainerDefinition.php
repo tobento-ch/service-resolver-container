e9ecca14532e5941a775b341a4e8405587468f5d
TOBENTOch <info@tobento.ch>
2023-11-29 19:25:52 +0100

    multiple changes

--- src/ContainerDefinition.php
+++ src/ContainerDefinition.php
@@ -20,6 +20,57 @@ use Tobento\Service\Container\DefinitionInterface as ContainerDefinitionInterfac
  * ContainerDefinition
  */
 class ContainerDefinition extends Definition implements ContainerDefinitionInterface
-{    
-    //
+{
+    /**
+     * Set the parameters.
+     *
+     * @param array<int|string, mixed> $parameters The parameters.
+     * @return static $this
+     */
+    public function with(array $parameters = []): static
+    {
+        $this->parameters = $parameters;
+        
+        return $this;
+    }
+    
+    /**
+     * Set the parameters.
+     *
+     * @param mixed ...$parameters The parameters.
+     * @return static $this
+     */
+    public function construct(...$parameters): static
+    {
+        $this->parameters = $parameters;
+        
+        return $this;
+    }
+    
+    /**
+     * Set a method to call with parameters.
+     *
+     * @param string $method The name of the method
+     * @param array<int|string, mixed> $parameters The parameters.
+     * @return static $this
+     */
+    public function callMethod(string $method, array $parameters = []): static
+    {
+        $this->methods[] = [$method, $parameters];
+        
+        return $this;
+    }
+    
+    /**
+     * Set if it a prototype, meaning returning always new instance.
+     *
+     * @param bool $prototype
+     * @return static $this
+     */
+    public function prototype(bool $prototype = true): static
+    {
+        $this->prototype = $prototype;
+        
+        return $this;
+    }
 }
